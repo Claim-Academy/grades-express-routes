3fde138d8410581d1f09ad39ff5abc4662ccde78
Manav Misra <manavm@visionwebsoft.com>
2023-02-03 12:41:45 -0600

    Remove logs

--- app/user/User.js
+++ app/user/User.js
@@ -25,10 +25,8 @@ const UserSchema = new Schema({
 });
 
 UserSchema.pre("save", async function (next) {
-  console.log("PRE SAVE");
   // * Only hash the password if it has been modified (or is new)
   if (this.isModified("password")) {
-    console.log(this.password, "PASSWORD");
     const generatedSalt = await bcrypt.genSalt(config.saltRounds);
     this.password = await bcrypt.hash(this.password, generatedSalt);
   }
